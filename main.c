6d4188b63c77345d0b5249e3c79368d6e2559042
Momin Shaikh <mominshaikh54849@gmail.com>
2025-10-13 16:48:08 +0000

    Necessary Functions Added

--- main.c
+++ main.c
@@ -15,3 +15,20 @@ int main(int argc,char **argv)
     //Using EXIT_SUCCESS is same as return 0;
     return EXIT_SUCCESS;
 }
+
+void lsh_loop(void)
+{
+    char *line; //to store the line read from stdin
+    char **args; //to store the command and its arguments
+    int status; //to check whether to continue the loop or not
+
+    do{
+        printf("> ");
+        line = lsh_read_line();//reading a line from stdin
+        args= lsh_split_line(line);//splitting the line into command and arguments
+        status = lsh_execute(args);//executing the command
+
+        free(line);
+        free(args);
+    } while (status);
+}
