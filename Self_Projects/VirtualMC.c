e4b8f7966b9c478ece6adef566e11762b28022b8
Momin Shaikh <mominshaikh54849@gmail.com>
2025-10-28 14:20:06 +0000

    Completed with the Hardware part

--- Self_Projects/VirtualMC.c
+++ Self_Projects/VirtualMC.c
@@ -32,3 +32,31 @@ enum {
 };
 
 uint16_t reg[R_COUNT]; //Each of size 16 bits
+
+enum {
+    OP_BR, /* Branch */
+    OP_ADD, /* Add */
+    OP_LD, /* Load */
+    OP_ST, /* Store */
+    OP_JSR, /* Jump to Subroutine */
+    OP_AND, /* Bitwise AND */
+    OP_LDR, /* Load Register */
+    OP_STR, /* Store Register */
+    OP_RTI, /* Return from Interrupt */
+    OP_NOT, /* Bitwise NOT */
+    OP_LDI, /* Load Indirect */
+    OP_STI, /* Store Indirect */
+    OP_JMP, /* Jump */
+    OP_RES, /* Reserved */
+    OP_LEA, /* Load Effective Address */
+    OP_TRAP /* Trap */
+};
+
+// Condition Flags
+enum {
+    FL_POS = 1 << 0, // P (bit 0: 0b001)
+    FL_ZRO = 1 << 1, // Z (bit 1: 0b010)
+    FL_NEG = 1 << 2  // N (bit 2: 0b100)
+};
+
+//We have Finished settitng up the Hardware components of our VM
