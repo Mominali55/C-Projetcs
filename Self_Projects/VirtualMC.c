ca7cec2c44f28683d401eea91e5a81d1f45e86ea
Momin Shaikh <mominshaikh54849@gmail.com>
2025-10-28 15:20:21 +0000

    Handaling Setup signals..chenking for argument(cmd)..specifying instrution for calcualtion register..

--- Self_Projects/VirtualMC.c
+++ Self_Projects/VirtualMC.c
@@ -60,3 +60,75 @@ enum {
 };
 
 //We have Finished settitng up the Hardware components of our VM
+
+int main(int argc, char *argv[]) //Taking command line argumnets
+{
+    //Setup signal handlers
+    signal(SIGINT,handle_interrupt);
+    disable_input_buffering();
+
+    //load argumnets
+
+    //Checking for Wrong Format or Insufficient arguments
+    if (argc < 2)
+    {
+        /*show usage string*/
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+
+    for(int i=1;i<argc;i++)
+    {
+        if(!read_image(argv[i]))
+        {
+            printf("Failed to load image: %s\n",argv[i]);
+            exit(1);
+        }
+    }
+
+    /*Since exactly one condition flag should be set at any given time,set the Z Flag*/
+    reg[R_COND] = FL_ZRO; //Using the Conditional flags from "enum" above
+
+    /*set the Pc to starting position*/
+    /*0x3000 is the default*/
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START; //using the Program counter from "enum" above
+
+    int running =1;
+    while (running)
+    {
+        /*FETCH*/
+        uint16_t instr =mem_read(reg[R_PC]++);  //important to post increment the PC **
+        uint16_t op = instr >> 12; //top 4 bits of the instruction
+
+        switch (op)
+        {
+        case OP_ADD:
+            {
+                /*destination register(DR)*/
+                uint16_t r0 =(instr >> 9) & 0x7;
+                /*first operand (SR1)*/
+                uint16_t r1 =(instr >> 6) & 0x7;
+                /*whether to use immediate mode*/
+                uint16_t imm_flag =(instr >> 5) & 0x1;
+
+                if(imm_flag)
+                {
+                    uint16_t imm5 =sign_extend(instr & 0x1F ,5);
+                    reg[r0] = reg[r1] + imm5;
+                }
+                else
+                {
+                    uint16_t r2 =instr & 0x7;
+                    reg[r0] = reg[r1] + reg[r2];
+                }
+
+                update_flags(r0);
+            }
+            break;
+        default:
+            break;
+        }
+    }
+
+}
